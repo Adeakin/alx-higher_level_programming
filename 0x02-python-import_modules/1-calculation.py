f757fa30861e53e6793e65b601f8c91501a5b7d8
Adeakin <abimbolaakinshilo@gmail.com>
2024-01-04 18:53:25 -0800

    task 2

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
 
 if __name__ == "__main__":
+    
     #Print the number of and list of arguments.
     import sys
-
-    calculate = len(sys.argv) - 1
-    if calculate == 0:
+    count = len(sys.argv) - 1
+    if count == 0:
         print("0 arguments.")
-    elif calculate == 1:
+    elif count == 1:
         print("1 argument:")
     else:
-        print("{} arguments:".format(calculate))
-    for i in range(calculate):
+        print("{} arguments:".format(count))
+    for i in range(count):
         print("{}: {}".format(i + 1, sys.argv[i + 1]))
