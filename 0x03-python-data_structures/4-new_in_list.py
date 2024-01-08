b27bbcbee0fadaffc98d3d58c90ad2b6c33725b6
Abimbola Akinshilo <abimbolaakinshilo@gmail.com>
2024-01-08 05:43:45 +0100

    Update 4-new_in_list.py

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -2,6 +2,7 @@
 # a function that replaces an element 
 # in a list at a specific position 
 # without modifying the original list (like in C).
+
 def new_in_list(my_list, idx, element):
     copy = my_list.copy()
     if  idx < 0 or idx > len(my_list) - 1:
