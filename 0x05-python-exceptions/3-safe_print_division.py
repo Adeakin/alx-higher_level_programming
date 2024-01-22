5f762f86acf8c88818abc7a6ba0900ac8141412b
Adeakin <abimbolaakinshilo@gmail.com>
2024-01-22 00:43:08 -0800

    divides 2 integers and prints the result

---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def safe_print_division(a, b):
+    """Divides 2 integers and prints the result.."""
+    try:
+        div = a / b
+    except (TypeError, ZeroDivisionError):
+        div = None
+    finally:
+        print("Inside result: {}".format(div))
+    return (div)
