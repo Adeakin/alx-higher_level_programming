ca17866021188fc2ce443a05b8f2201220c4207c
Adeakin <abimbolaakinshilo@gmail.com>
2024-01-23 19:09:41 -0800

    class Square that defines a square by: (based on 1-square.py) with raise value|type error|def area(self)

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-"""Define a class Square"""
+"""Define a class Square."""
 
 
 class Square:
@@ -13,9 +13,9 @@ class Square:
         size (int): The size of the new square.
         """
         if not isinstance(size, int):
-            raise TypeError(size must be an integer)
+            raise TypeError("size must be an integer")
         elif size < 0:
-            raise ValueError(size must be >= 0)
+            raise ValueError("size must be >= 0")
         self.__size = size
 
         def area(self):
