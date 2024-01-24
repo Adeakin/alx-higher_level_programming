47e1905ba4d78d52edd2c30f6b5212bd24d2d58f
Adeakin <abimbolaakinshilo@gmail.com>
2024-01-23 18:25:13 -0800

    class Square that defines a square by: (based on 1-square.py) with raise value|type error

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -12,7 +12,7 @@ class Square:
         Args:
         size (int): The size of the new square.
         """
-        if not  isinstance(size, int):
+        if not isinstance(size, int):
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
